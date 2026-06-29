238e984e8cfac796960a3d17d4bb569f99371661
bankai08 <shinjo.dallakoti022@apexcollege.edu.np>
2026-06-29 17:08:07 +0545

    feat: add core layout components (header, footer, container, layouts)

---
+++ frontend/src/components/Container.jsx
@@ -0,0 +1,26 @@
+// This componet is for just to structure the website
+
+const Container = ({ children }) => {
+  return (
+    <div className="flex flex-col items-center justify-center w-4/5 mx-auto">
+      <div
+        className="absolute inset-0 -z-1"
+        style={{
+          background: "#ffffff",
+          backgroundImage: `
+        radial-gradient(
+          circle at top right,
+          rgba(70, 130, 180, 0.5),
+          transparent 70%
+        )
+      `,
+          filter: "blur(80px)",
+          backgroundRepeat: "no-repeat",
+        }}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default Container;
---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white py-6">
+      <div className="container mx-auto text-center">
+        <p>© 2025 H₂Otronics. All rights reserved.</p>
+        <p>
+          Follow us:
+          <a href="https://github.com/AquaSense" target="_blank" className="underline ml-2">
+            GitHub
+          </a>
+          ,
+          <a href="https://www.linkedin.com/in/hard-tom-988252394/" target="_blank" className="underline ml-2">
+            LinkedIn
+          </a>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
